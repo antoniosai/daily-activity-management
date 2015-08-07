12475bc864950840ce72e9f5f270fbe32e09f8ec
Antonio Saiful <antoniosaiful10@gmail.com>
2015-08-07 13:38:25 +0700

    another update

--- app/controllers/ExportController.php
+++ app/controllers/ExportController.php
@@ -4,32 +4,38 @@ class ExportController extends \BaseController {
 
 	public function exportToPdf(){
 
+
+		$data = DB::table('logbooks')
+		->join('priorities', 'logbooks.priorities_id', '=', 'priorities.id')
+		->join('users', 'logbooks.user_id', '=', 'users.id');
+		
 		$fromDate = Input::get('fromDate');
 		$toDate = Input::get('toDate');
 		$sid = Input::get('sid');
 		$oid = Input::get('oid');
 
-		if ($oid) {
+		if ($oid == 'all') {
+			$opName = "Semua Operator";
+		} else if ($oid) {
 			$user = User::find($oid);
 			$opName = $user->first_name . ' ' . $user->last_name;
+			$data->where('logbooks.user_id', '=', $oid);
 		}
 
-		if ($sid) {
+		if ($sid == 'all') {
+		 	$sName = "Semua Status";
+		 } else if ($sid) {
 			$status = Priorities::find($sid);
 			$sName = $status->description;
+			$data->where('logbooks.priorities_id', '=', $sid);
 		}
 
-		$data = DB::table('logbooks')
-		->join('priorities', 'logbooks.priorities_id', '=', 'priorities.id')
-		->join('users', 'logbooks.user_id', '=', 'users.id')
-		->where('logbooks.priorities_id', '=', $sid)
-		->where('logbooks.user_id', '=', $oid)
-		->select('logbooks.user_id', 'users.id', 'logbooks.created_at', 'logbooks.title','logbooks.deskripsi','logbooks.priorities_id','priorities.description as priority','users.first_name', 'users.last_name')
-		->whereBetween('logbooks.created_at', array($fromDate, $toDate))
-		->orderBy('logbooks.created_at', 'asc');
+		$query = $data->select('logbooks.user_id', 'users.id', 'logbooks.created_at', 'logbooks.title','logbooks.deskripsi','logbooks.priorities_id','priorities.description as priority','users.first_name', 'users.last_name')
+					  ->whereBetween('logbooks.created_at', array($fromDate, $toDate))
+		           	  ->orderBy('logbooks.created_at', 'asc');
 
 		$logbook = [
-			'data' => $data->get(),
+			'data' => $query->get(),
 			'fromDate' => $fromDate,
 			'toDate' => $toDate,
 			'sName' => $sName,
